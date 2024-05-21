d893d39c5cde014bb6c2e470eb41d9e2a68c6f83
DenisGenero <denisgenero@live.com.ar>
2024-05-21 17:09:16 -0300

    Material para clase 5

---
+++ Workspace/Clase5/Core/Inc/asm_func.h
@@ -0,0 +1,15 @@
+/*
+ * asm_func.h
+ *
+ *  Created on: May 15, 2024
+ *      Author: denis
+ */
+
+#ifndef INC_ASM_FUNC_H_
+#define INC_ASM_FUNC_H_
+
+/* Clase 5*/
+void MediaMovil(uint16_t *vectorIn, uint16_t *vectorOut, uint32_t longitud);
+void asm_MediaMovil(uint16_t *vectorIn, uint16_t *vectorOut, uint32_t longitud);
+
+#endif /* INC_ASM_FUNC_H_ */
