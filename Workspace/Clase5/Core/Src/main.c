41286b017d068215cee34c013d02ee0200a2a3d9
DenisGenero <denisgenero@live.com.ar>
2024-05-29 18:14:03 -0300

    acomodo de identación

--- Workspace/Clase5/Core/Src/main.c
+++ Workspace/Clase5/Core/Src/main.c
@@ -67,21 +67,21 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-	DWT->CTRL |= 1 << DWT_CTRL_CYCCNTENA_Pos;
-	uint32_t ciclos_C, ciclos_ASM;
+    DWT->CTRL |= 1 << DWT_CTRL_CYCCNTENA_Pos;
+    uint32_t ciclos_C, ciclos_ASM;
 
-	uint16_t senoRuido[] = { };
-	uint32_t lon = sizeof(senoRuido)/sizeof(senoRuido[0]);
-	uint16_t senoFilt_C[lon];
-	uint16_t senoFilt_ASM[lon];
+    uint16_t senoRuido[] = { };
+    uint32_t lon = sizeof(senoRuido)/sizeof(senoRuido[0]);
+    uint16_t senoFilt_C[lon];
+    uint16_t senoFilt_ASM[lon];
 
-	DWT->CYCCNT = 0;
-	MediaMovil(senoRuido, senoFilt_C, lon);
-	ciclos_C = DWT->CYCCNT;
+    DWT->CYCCNT = 0;
+    MediaMovil(senoRuido, senoFilt_C, lon);
+    ciclos_C = DWT->CYCCNT;
 
-	DWT->CYCCNT = 0;
-	asm_MediaMovil(senoRuido, senoFilt_ASM, lon);
-	ciclos_ASM = DWT->CYCCNT;
+    DWT->CYCCNT = 0;
+    asm_MediaMovil(senoRuido, senoFilt_ASM, lon);
+    ciclos_ASM = DWT->CYCCNT;
 
   /* USER CODE END 1 */
 
@@ -245,26 +245,26 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 
 void MediaMovil(uint16_t *vectorIn, uint16_t *vectorOut, uint32_t longitud){
-	int32_t limiteSup, limiteInf;
-	uint8_t SampleOffset = 4;
-	uint8_t L = 2*SampleOffset + 1;
-	for (int32_t i = longitud; i > 0; i--){
-		// Se define el límite superior para no tomar muestras fuera del vector
-		limiteSup = (i - 1) + SampleOffset;
-		if ( limiteSup > (longitud - 1)){
-			limiteSup = longitud - 1;
-		}
-		// Se define el límite inferior para no tomar muestras fuera del vector
-		limiteInf = (i - 1) - SampleOffset;
-		if ( limiteInf < 0){
-			limiteInf = 0;
-		}
-		vectorOut[i-1] = 0;
-		for (int32_t j = limiteSup; j >= limiteInf; j--){
-			vectorOut[i-1] += vectorIn[j];
-		}
-		vectorOut[i-1] /= L;
-	}
+    int32_t limiteSup, limiteInf;
+    uint8_t SampleOffset = 4;
+    uint8_t L = 2*SampleOffset + 1;
+    for (int32_t i = longitud; i > 0; i--){
+        // Se define el límite superior para no tomar muestras fuera del vector
+        limiteSup = (i - 1) + SampleOffset;
+        if ( limiteSup > (longitud - 1)){
+            limiteSup = longitud - 1;
+        }
+        // Se define el límite inferior para no tomar muestras fuera del vector
+        limiteInf = (i - 1) - SampleOffset;
+        if ( limiteInf < 0){
+            limiteInf = 0;
+        }
+        vectorOut[i-1] = 0;
+        for (int32_t j = limiteSup; j >= limiteInf; j--){
+            vectorOut[i-1] += vectorIn[j];
+        }
+        vectorOut[i-1] /= L;
+    }
 }
 
 
